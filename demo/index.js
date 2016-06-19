f81113934a5314f0ea4e83233eb4320dbcf92b05
anvaka <anvaka@gmail.com>
2016-06-18 20:41:59 -0700

    Added custom dot import for demo. Just for debugging

--- demo/index.js
+++ demo/index.js
@@ -1,25 +1,13 @@
+var render = require('ngraph.svg');
 var getColor = require('./getColor.js');
+var createWhisper = require('../');
+var fromdot = require('ngraph.fromdot');
 
-var graph = require('miserables');
-var whisper = require('../')(graph);
+var defaultGraph = require('miserables');
+var currentApp = app(defaultGraph);
 
-function onStep(stepsCount) {
-  for (var i = 0; i < stepsCount; ++i) {
-    whisper.step();
-  }
-  // Once change rate is close to 0, the algorithm has converged.
-  console.log(whisper.getChangeRate() + '% nodes received a new class');
-  // Next iteration of renderNode() will use whisper.getClass(node.id) and
-  // will render it on the screen.
-}
-
-// We use svg renderer, but this could be any renderer
-renderGraph(graph);
-
-function renderGraph(graph) {
-  var render = require('ngraph.svg');
-  var svg = render.svg;
-
+function app(graph) {
+  var whisper = createWhisper(graph);
   var renderer = render(graph, {
     physics: {
       springLength : 35,
@@ -29,42 +17,78 @@ function renderGraph(graph) {
     }
   });
 
-  renderer.node(createNode).placeNode(renderNode);
+  var api = {
+    step: step,
+    dispose: dispose
+  };
 
-  renderer.run();
+  renderGraph();
 
-  function renderNode(ui, pos, node) {
-    var currentClass = whisper.getClass(node.id);
-    if (ui.label !== currentClass) {
-      ui.label = currentClass;
-      ui.labelEl.text(currentClass);
-      ui.circleEl.attr('fill', getColor(currentClass));
-    }
-    ui.attr('transform', 'translate(' + (pos.x ) + ',' + (pos.y) + ')');
+  return api;
+
+  function dispose() {
+    renderer.dispose();
+    renderer.svgRoot.parentNode.removeChild(renderer.svgRoot);
   }
 
-  function createNode(node) {
-    var ui = svg('g');
-        // Create SVG text element with user id as content
-    var label = svg('text').attr('y', '-8px');
-    var circle = svg('circle', {
-      r: 7,
-      stroke: '#fff',
-      fill: '#ff00ef',
-      'stroke-width': '1.5px'
-    });
-    ui.append(label);
-    ui.append(circle);
+  function step() {
+    whisper.step();
+    // Once change rate is close to 0, the algorithm has converged.
+    console.log(whisper.getChangeRate() + '% nodes received a new class');
+    // Next iteration of renderNode() will use whisper.getClass(node.id) and
+    // will render it on the screen.
+  }
 
-    // yeah, ugly. Storing label dom element, so that we can update labels later
-    ui.labelEl = label;
-    ui.circleEl = circle;
-    return ui;
+  function renderGraph() {
+    var svg = render.svg;
+
+    renderer.node(createNode).placeNode(renderNode);
+    renderer.run();
+
+    function renderNode(ui, pos, node) {
+      var currentClass = whisper.getClass(node.id);
+      if (ui.label !== currentClass) {
+        ui.label = currentClass;
+        ui.labelEl.text(currentClass);
+        ui.circleEl.attr('fill', getColor(currentClass));
+      }
+      ui.attr('transform', 'translate(' + (pos.x ) + ',' + (pos.y) + ')');
+    }
+
+    function createNode(node) {
+      var ui = svg('g');
+          // Create SVG text element with user id as content
+      var label = svg('text').attr('y', '-8px');
+      var circle = svg('circle', {
+        r: 7,
+        stroke: '#fff',
+        fill: '#ff00ef',
+        'stroke-width': '1.5px'
+      });
+      ui.append(label);
+      ui.append(circle);
+
+      // yeah, ugly. Storing label dom element, so that we can update labels later
+      ui.labelEl = label;
+      ui.circleEl = circle;
+      return ui;
+    }
   }
 }
 
+
+
 document.getElementById('step').addEventListener('click', function(e) {
   e.preventDefault();
-  onStep(1);
+  currentApp.step();
+});
+
+document.getElementById('loaddot').addEventListener('click', function(e) {
+  e.preventDefault();
+  currentApp.dispose();
+
+  var dot = document.getElementById('custom-dot').value;
+  var graph = fromdot(dot)
+  currentApp = app(graph);
 });
 
